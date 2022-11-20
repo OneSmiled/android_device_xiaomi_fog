ea3da1d1ad2558ec215738d4859c8a36e69fffe3
OneSmiled <115576535+OneSmiled@users.noreply.github.com>
2022-11-21 00:35:19 +0500

    Update fog.mk

--- fog.mk
+++ fog.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
-PRODUCT_BRAND:=Xiaomi
-PRODUCT_DEVICE:=fog
-PRODUCT_NAME:=fog
+PRODUCT_BRAND :=Xiaomi
+PRODUCT_DEVICE :=fog
+PRODUCT_NAME :=fog
